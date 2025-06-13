839086fc2694e1f8aeb826b833029b5476e2f44d
Yash Maurya <yashmaurya969@gmail.com>
2025-06-13 08:30:05 +0000

    Delete malicious.py

--- malicious.py
+++
@@ -1,8 +0,0 @@
-# test_sqli.py
-def unsafe_query(user_input):
-    import sqlite3
-    conn = sqlite3.connect("example.db")
-    cursor = conn.cursor()
-    query = f"SELECT * FROM users WHERE username = '{user_input}'"  # UNSAFE - SQLi
-    cursor.execute(query)
-    return cursor.fetchall()
