f453498311082ae94b6c983d55b0356f268b2e17
Yash Maurya <yashmaurya969@gmail.com>
2025-08-08 22:41:37 +0530

    Delete XSS.js

--- XSS.js
+++
@@ -1,7 +0,0 @@
-// Simulated XSS vulnerability (DO NOT USE IN PRODUCTION)
-
-const urlParams = new URLSearchParams(window.location.search);
-const userInput = urlParams.get("name");
-
-// This line is vulnerable to reflected XSS
-document.getElementById("output").innerHTML = "Hello " + userInput;
